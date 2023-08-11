2dab766e7685299a1749143c9eb7dae9e8ead54d
NARD <110368170+CocoShesh@users.noreply.github.com>
2023-08-11 08:03:53 -0700

    Update index.js

--- index.js
+++ index.js
@@ -27,21 +27,22 @@ function dictionary() {
         const obj = data[0];
 
         header.innerHTML = obj.word;
-        phonetics.innerHTML = obj.phonetics[0].text;
-        partsOfspeech.innerHTML = obj.meanings[0].partOfSpeech;
-        meaningHeader.innerHTML = obj.meanings[0].definitions[0].definition;
+        phonetics.innerHTML = obj.phonetics[0]?.text || "";
+        partsOfspeech.innerHTML = obj.meanings[0]?.partOfSpeech || "";
+        meaningHeader.innerHTML =
+          obj.meanings[0]?.definitions[0]?.definition || "";
         meaningHeader.style.fontWeight = "bold";
-        meaning2.innerHTML = obj.meanings[0].definitions[1].definition;
-        meaning3.innerHTML = obj.meanings[0].definitions[2].definition;
+        meaning2.innerHTML = obj.meanings[0]?.definitions[1]?.definition || "";
+        meaning3.innerHTML = obj.meanings[0]?.definitions[2]?.definition || "";
 
-        const synonymsList = obj.meanings[0].synonyms;
+        const synonymsList = obj.meanings[0]?.synonyms || "";
         if (synonymsList.length > 0) {
           synonyms.innerHTML = "Synonyms: " + synonymsList.join(", ");
         } else {
           synonyms.innerHTML = "No synonyms found.";
         }
 
-        const antonymsList = obj.meanings[0].antonyms;
+        const antonymsList = obj.meanings[0]?.antonyms || "";
         if (antonymsList.length > 0) {
           antonyms.innerHTML = "Antonyms: " + antonymsList.join(", ");
         } else {
@@ -49,9 +50,10 @@ function dictionary() {
         }
 
         if (obj.meanings[1]) {
-          partOfSpeech2.innerHTML = obj.meanings[1].partOfSpeech;
+          partOfSpeech2.innerHTML = obj.meanings[1]?.partOfSpeech || "";
           partOfSpeech2.style.fontWeight = "bold";
-          second_meaning1.innerHTML = obj.meanings[1].definitions[0].definition;
+          second_meaning1.innerHTML =
+            obj.meanings[1]?.definitions[0].definition || "";
           second_meaning1.style.display = "block";
         } else {
           partOfSpeech2.innerHTML = "";
@@ -70,20 +72,7 @@ function dictionary() {
         messageWarning.innerHTML = "";
         resolutionWarning.innerHTML = "";
       } else {
-        header.innerHTML = "";
-        phonetics.innerHTML = "";
-        partsOfspeech.innerHTML = "";
-        meaningHeader.innerHTML = "";
-        meaning1.innerHTML = "";
-        meaning2.innerHTML = "";
-        meaning3.innerHTML = "";
-        synonyms.innerHTML = "";
-        antonyms.innerHTML = "";
-        partOfSpeech2.innerHTML = "";
-        second_meaning1.innerHTML = "";
-        source.innerHTML = "";
-        link.innerHTML = "";
-
+        resetElements();
         titleWarning.innerHTML = "No Definitions Found";
         messageWarning.innerHTML =
           "Sorry pal, we couldn't find definitions for the word you were looking for.";
@@ -94,4 +83,25 @@ function dictionary() {
     .catch(error => {
       console.log("Something went wrong", error);
     });
+}
+function resetElements() {
+  const elementsToReset = [
+    header,
+    phonetics,
+    partsOfspeech,
+    meaning1,
+    meaning2,
+    meaning3,
+    meaningHeader,
+    synonyms,
+    antonyms,
+    partOfSpeech2,
+    second_meaning1,
+    source,
+    link,
+  ];
+
+  elementsToReset.forEach(element => {
+    element.innerHTML = "";
+  });
 }
